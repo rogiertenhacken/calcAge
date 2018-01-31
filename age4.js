46949d90af7d3f42ca2aa02a39c8cde594b43fb4
Rogier <rogiertenhacken@gmail.com>
2018-01-31 09:46:25 +0100

    Update age4.js

--- age4.js
+++ age4.js
@@ -22,16 +22,3 @@ const calcAge = () => {
    document.getElementById("output").innerHTML = 'ON EARTH FOR ' + yearsSinceBirth2 + ' YEARS';
  }
  };
-
-// maybe a new output field (using #outputTwo in the html-file) with remaining months to birthday. using the Math.abs or var posNum = (num < 0) ? num * -1 : num; // if num is negative multiple by negative (https://stackoverflow.com/questions/4652104/convert-a-negative-number-to-a-positive-one-in-javascript)
-
-// if the new outfield works, also try and use with the code below:
-
-/* if (currentDay < dayOfBirth && currentMonth <= monthOfBirth) {
-   document.getElementById("output").innerHTML = 'ON EARTH FOR ' + yearsSinceBirth2 + ' YEARS ' + ' (and you have ' +  monthsSinceBirth + ' months ' + ' and ' + daysSinceBirth + ' days' + "<br />" + ' left to your next birthday)';
-} else if (monthsSinceBirth === 0){
-  document.getElementById("output").innerHTML = 'ON EARTH FOR ' + yearsSinceBirth + ' YEARS ' + ' AND ' + monthsSinceBirth + ' MONTH '; // if only month past not more months
-} else {
-  document.getElementById("output").innerHTML = 'ON EARTH FOR ' + yearsSinceBirth2 + ' YEARS ' + ' AND ' + monthsSinceBirth + ' MONTHS LEFT TO YOUR NEXT BIRTHDAY ';
-}
-}; */
